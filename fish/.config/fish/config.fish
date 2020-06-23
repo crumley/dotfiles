a48f6fd125094d1c28c11a56397148c875376a78
Ryan Crumley <crumley@gmail.com>
2020-06-23 17:40:42 -0500

    Improve based on new compy setup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -27,3 +27,9 @@ alias r="ripgrep"
 alias ports "sudo lsof -i -n -P | grep TCP"
 
 source ~/.iterm2_shell_integration.fish
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
