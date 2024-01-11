3f54e8b5c21e9da55c481d3a1797a241b9662fcd
Ryan Crumley <crumley@gmail.com>
2024-01-11 15:52:21 -0600

    Format wm and add more menu options

--- hammerspoon/.hammerspoon/wm.lua
+++ hammerspoon/.hammerspoon/wm.lua
@@ -1,4 +1,3 @@
-
 local chooser = require('hs.chooser')
 local fnutils = require('hs.fnutils')
 local logger = require('hs.logger')
@@ -7,64 +6,66 @@ local m = {}
 m.logger = logger.new('chooser', 'debug')
 
 local actions = {
-    restart_yabai = function (x) 
+    restart_yabai = function (x)
         yabai("--restart-service")
     end,
 
-    off_yabai = function (x) 
+    off_yabai = function (x)
         yabai("--stop-service")
     end,
-    
-    on_yabai = function (x) 
+
+    on_yabai = function (x)
         yabai("--start-service")
     end,
-    
-    rotate = function (x) 
-        yabai("-m space --rotate 90")
-    end,
-    
-    mirror_x = function (x) 
+
+    rotate = function (x)
         yabai("-m space --rotate 90")
     end,
 
-    mirror_y = function (x) 
+    mirror_x = function (x)
+        yabai("-m space --rotate 90")
+    end,
+
+    mirror_y = function (x)
         yabaish("/opt/homebrew/bin/yabai -m space --mirror y-axis")
     end,
 
     left = function ()
-        yabaish("/opt/homebrew/bin/yabai -m window --resize right:-20:0 || /opt/homebrew/bin/yabai -m window --resize left:-20:0")
+        yabaish(
+            "/opt/homebrew/bin/yabai -m window --resize right:-20:0 || /opt/homebrew/bin/yabai -m window --resize left:-20:0")
     end,
 
     right = function ()
-        yabaish("/opt/homebrew/bin/yabai -m window --resize right:20:0 || /opt/homebrew/bin/yabai -m window --resize left:20:0")
+        yabaish(
+            "/opt/homebrew/bin/yabai -m window --resize right:20:0 || /opt/homebrew/bin/yabai -m window --resize left:20:0")
     end,
 }
 
-function split (inputstr, sep)
+function split(inputstr, sep)
     if sep == nil then
-            sep = "%s"
+        sep = "%s"
     end
-    local t={}
-    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-            table.insert(t, str)
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
     end
     return t
 end
 
 function yabai(cli)
     -- Runs in background very fast
-    hs.task.new("/opt/homebrew/bin/yabai", nil, function(ud, ...)
-      print("stream", hs.inspect(table.pack(...)))
-      return true
+    hs.task.new("/opt/homebrew/bin/yabai", nil, function (ud, ...)
+        print("stream", hs.inspect(table.pack(...)))
+        return true
     end, split(cli)):start()
 end
 
 function yabaish(cli)
     -- Runs in background very fast
-    hs.task.new("/bin/bash", nil, function(ud, ...)
-      print("stream", hs.inspect(table.pack(...)))
-      return true
-    end, {"-c", cli}):start()
+    hs.task.new("/bin/bash", nil, function (ud, ...)
+        print("stream", hs.inspect(table.pack(...)))
+        return true
+    end, { "-c", cli }):start()
 end
 
 function m:init()
@@ -75,27 +76,44 @@ function m:init()
     m.horizontal_ratios = { 0.1, 0.3, 0.5, 0.7, 0.9 }
     m.horizontal_index = 3
 
-    m.chooser = chooser.new(function ( selection ) 
+    m.chooser = chooser.new(function (selection)
         if selection ~= nil then
             m:action(selection["uuid"])
         end
     end)
 
-    m.chooser:choices( {
+    m.chooser:choices({
         {
-         ["text"] = "Restart Yabai",
-         ["subText"] = "",
-         ["uuid"] = "restart_yabai"
+            ["text"] = "Rotate",
+            ["subText"] = "",
+            ["uuid"] = "rotate"
         },
-        { ["text"] = "Yabai off",
-          ["subText"] = "Turn off",
-          ["uuid"] = "off_yabai"
+        {
+            ["text"] = "Mirror X",
+            ["subText"] = "",
+            ["uuid"] = "mirror_x"
         },
-        { ["text"] = "Yabai on",
-          ["subText"] = "Turn on",
-          ["uuid"] = "on_yabai"
+        {
+            ["text"] = "Mirror Y",
+            ["subText"] = "",
+            ["uuid"] = "mirror_y"
         },
-    } )
+        {
+            ["text"] = "Restart Yabai",
+            ["subText"] = "",
+            ["uuid"] = "restart_yabai"
+        },
+        {
+            ["text"] = "Yabai off",
+            ["subText"] = "Turn off",
+            ["uuid"] = "off_yabai"
+        },
+        {
+            ["text"] = "Yabai on",
+            ["subText"] = "Turn on",
+            ["uuid"] = "on_yabai"
+        },
+    })
 end
 
 function m:action(actionName)
